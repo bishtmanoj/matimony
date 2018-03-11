f69139805cb0f45f674b6e4bfd51ab0057454050
= <you@example.com>
2018-03-11 19:55:47 +0530

    mend

--- resources/views/account/picture.blade.php
+++ resources/views/account/picture.blade.php
@@ -4,6 +4,6 @@
 <a href="{{ route('profile.edit','picture') }}" class="btn btn-default">Edit Picture</a>
 @endslot
 <div class="col-md-4">
-<img class="img img-responsive" src="https://grand-vet.ru/wp-content/uploads/2017/11/default-avatar-250x250.png"/>
+<img class="img img-responsive" src="{{ asset('uploads/profiles/'.Auth::user()->profile_picture) }}"/>
 </div>
 @endcomponent
